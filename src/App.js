4b1cbc7ed219d3b0fa06b080e3c0c395227aa2f2
Joao Paulo <joaoded@hotmail.com>
2018-03-27 11:35:07 -0300

    Adding bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
 
 // HomePage ...
-import HomePage from './components/pages/HomePage';
+// import HomePage from './components/pages/HomePage';
 
 const App = () => (
-  <HomePage />
+  <div>
+    <Container>
+        <Row>
+        <div>
+          <Col><h2 className="space-top">Prazer em ter você aqui. Nós somos a repub.</h2></Col>
+          <Col><h5>Aqui, você pode encontrar vagas
+             para morar em república ou simplesmente publicar a sua!</h5></Col>
+        </div>
+        </Row>
+      </Container>
+  </div>
 );
 
 export default App;
